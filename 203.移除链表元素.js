c00ff68c131ae2fc6bbabd8e9a4821b9fc36b23a
wei <wei@weideMacBook-Pro.local>
2025-08-18 21:07:34 +0800

    203.移除链表元素

---
+++ 203.移除链表元素.js
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=203 lang=javascript
+ * @lcpr version=30202
+ *
+ * [203] 移除链表元素
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+    let dummy = new ListNode(-1, head);
+    let p = dummy;
+    while (p.next) {
+      if (p.next.val === val) {
+        p.next = p.next.next;
+      } else {
+        p = p.next;
+      }
+      
+    }
+    return dummy.next;
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,6,3,4,5,6]\n6\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n1\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [7,7,7,7]\n7\n
+// @lcpr case=end
+
+ */
+
